91447b3b9510ee12264ccad804a06bb723810ea5
TanyaSamal <solesytto@gmail.com>
2018-09-17 15:12:39 +0300

    resolve factorial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let count = 0;
+  for(let i = 1; i <= number; i++){
+      if (i % 5 == 0){
+          let divFive = i;
+          while(divFive % 5 == 0){
+              divFive /= 5;
+              count++;
+          }
+      }
+  }
+  return count;
 }
